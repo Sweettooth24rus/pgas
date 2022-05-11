9b99e726bfa98d825956a99e1fd8ef196d79fb54
Konstantin <32872379+Sweettooth24rus@users.noreply.github.com>
2022-05-11 17:09:09 +0700

    Initial commit

---
+++ src/main/java/com/kkoz/pgas/PgasApplication.java
@@ -0,0 +1,13 @@
+package com.kkoz.pgas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkoz/pgas/PgasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkoz.pgas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
